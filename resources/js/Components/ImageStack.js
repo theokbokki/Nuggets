6cf43b0df0de380c510df57473496a1427697e2b
théoo <90846584+theokbokki@users.noreply.github.com>
2025-06-01 20:26:34 +0200

    Make the image-stack component more polished

--- resources/js/Components/ImageStack.js
+++ resources/js/Components/ImageStack.js
@@ -13,29 +13,37 @@ export default class ImageStack {
 
     getElements() {
         this.images = this.el.querySelectorAll(".image-stack__img");
+        this.imagesContainer = this.el.querySelector(".image-stack__images");
         this.nugget = this.el.closest(".nugget");
         this.bg = this.el.querySelector(".image-stack__bg");
     }
 
     setEvents() {
-        this.el.addEventListener("click", this.open.bind(this));
-
         this.nugget.addEventListener("click", (e) => {
-            if (!this.el.contains(e.target)) {
+            const isClickInsideImages = this.imagesContainer.contains(e.target);
+            const isClickInsideBg = this.bg.contains(e.target);
+            const isOpen = this.el.classList.contains("image-stack--open");
+
+            if (!isOpen && isClickInsideImages) {
+                this.open();
+            } else if (isOpen && !isClickInsideImages && !isClickInsideBg) {
                 this.close();
             }
         });
     }
 
     open() {
-        const state = Flip.getState(this.images);
+        const state = Flip.getState(this.images, {
+            props: "opacity",
+        });
 
-        this.el.classList.add("image-stack--clicked");
+        this.el.classList.add("image-stack--open");
 
         gsap.to(this.bg, {
             width: "256px",
             height: "184px",
-            backgroundColor: "rgb(0, 0, 0, .15)",
+            backgroundColor: "rgb(255, 255, 255, .15)",
+            boxShadow: "0 4px 16px -8px rgb(0,0,0,0.35)",
             duration: 0.9,
             ease: "elastic.out(1, 0.6)",
         });
@@ -46,30 +54,33 @@ export default class ImageStack {
             ease: "elastic.out(1, 0.6)",
             stagger: 0.02,
             scale: true,
-            props: "backgroundColor",
+            props: "opacity",
         });
     }
 
     close() {
-        const state = Flip.getState(this.images);
+        const state = Flip.getState(this.images, {
+            props: "opacity, filter",
+        });
 
-        this.el.classList.remove("image-stack--clicked");
+        this.el.classList.remove("image-stack--open");
 
         gsap.to(this.bg, {
             width: "0",
             height: "0",
-            backgroundColor: "rgb(0, 0, 0, 0)",
+            backgroundColor: "rgb(255, 255, 255, 0)",
+            boxShadow: "0 4px 16px -16px rgb(0,0,0,0)",
             duration: 0.7,
-            ease: "power2.inOut",
+            ease: "power3.inOut",
         });
 
         Flip.from(state, {
             absolute: true,
             duration: 0.5,
-            ease: "power2.inOut",
+            ease: "power3.inOut",
             stagger: 0.02,
             scale: true,
-            props: "backgroundColor",
+            props: "opacity, filter",
         });
     }
 }
