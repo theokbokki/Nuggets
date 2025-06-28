19537561729690820b596dc63dcdb172b015935b
théoo <90846584+theokbokki@users.noreply.github.com>
2025-06-28 14:51:28 +0200

    Handle cursor on page load correctly

--- resources/js/Components/Cursor.js
+++ resources/js/Components/Cursor.js
@@ -32,6 +32,8 @@ export default class Cursor {
 
         this.currentBehaviour = null;
 
+        this.hasMoved = false;
+
         this.width = getCssValue(this.el, 'width', true);
         this.height = getCssValue(this.el, 'height', true);
         this.radius = getCssValue(this.el, 'border-radius', true);
@@ -44,30 +46,11 @@ export default class Cursor {
 
     handleMouseMove(e) {
         this.mouse = { x: e.clientX, y: e.clientY };
+        this.hasMoved = true;
     }
 
     animate() {
-        this.nuggets.forEach((nugget) => {
-            const bounds = nugget.getBoundingClientRect();
-
-            if (isMouseOverZone(this.mouse.x, this.mouse.y, bounds, -16)) {
-                gsap.to(this.el, {
-                    opacity: 0,
-                    duration: .5,
-                    ease: "power3.out",
-                });
-
-                document.documentElement.style.cursor = 'unset';
-            } else {
-                gsap.to(this.el, {
-                    opacity: 1,
-                    duration: .5,
-                    ease: "power3.out",
-                });
-
-                document.documentElement.style.cursor = 'none';
-            }
-        });
+        this.handleMouseOpacity();
 
         for (const behaviour of this.behaviours) {
             if (this.currentBehaviour?.isActive()) {
@@ -86,20 +69,58 @@ export default class Cursor {
         }
 
         if (!this.currentBehaviour) {
-            gsap.to(this.el, {
-                x: this.mouse.x - this.width / 2,
-                y: this.mouse.y - this.height / 2,
-                width: this.width,
-                height: this.height,
-                borderRadius: this.radius,
-                backgroundColor: this.backgroundColor,
-                duration: 0.15,
-                ease: "power2.out",
-            });
+            if (this.hasMoved && !this.firstMoveHandled) {
+                gsap.set(this.el, {
+                    x: this.mouse.x - this.width / 2,
+                    y: this.mouse.y - this.height / 2,
+                });
+
+                this.firstMoveHandled = true;
+            } else if (this.firstMoveHandled) {
+                gsap.to(this.el, {
+                    x: this.mouse.x - this.width / 2,
+                    y: this.mouse.y - this.height / 2,
+                    width: this.width,
+                    height: this.height,
+                    borderRadius: this.radius,
+                    backgroundColor: this.backgroundColor,
+                    duration: 0.15,
+                    ease: "power2.out",
+                });
+            }
         }
 
         this.currentBehaviour?.whileBehaviour();
 
         requestAnimationFrame(() => this.animate());
+    }
+
+    handleMouseOpacity() {
+        if (!this.hasMoved) {
+            return;
+        }
+
+        this.nuggets.forEach((nugget) => {
+            const bounds = nugget.getBoundingClientRect();
+
+            if (isMouseOverZone(this.mouse.x, this.mouse.y, bounds, -16)) {
+                gsap.to(this.el, {
+                    opacity: 0,
+                    duration: .5,
+                    ease: "power3.out",
+                });
+
+                document.documentElement.style.cursor = 'unset';
+                return;
+            }
+
+            gsap.to(this.el, {
+                opacity: 1,
+                duration: .5,
+                ease: "power3.out",
+            });
+
+            document.documentElement.style.cursor = 'none';
+        });
     }
 }
